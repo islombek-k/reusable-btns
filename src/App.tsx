db323574d69221d78a66a5cabf6e8684997bd97c
islombek-k <islomaliy74@gmail.com>
2023-08-28 10:56:50 +0500

    show all btns

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,44 @@ import { SocialButton } from "./components/social-button";
 
 function App() {
   return (
-    <div style={{display: "flex", gap: "12px"}}>
+    <div style={{ display: "flex", gap: "12px" }}>
+      <Button
+        variant="orangeClassic"
+        onClick={() => console.log("1st btn clicked")}
+      >
+        Orange Classic
+      </Button>
+      <div style={{backgroundColor: "#000", padding: "15px"}}>
+        <Button
+          variant="clearOutlined"
+          onClick={() => console.log("1st btn clicked")}
+        >
+          Clear Outlined
+        </Button>
+
+        <span style={{color: "#fff", marginLeft: "10px"}}>Bu yaxshi ko'rinishi uchun background berib qo'yganman</span>
+      </div>
       <Button
         variant="blackClassic"
         onClick={() => console.log("1st btn clicked")}
       >
-        Normal btn
+        Black Classic
       </Button>
+      <Button
+        variant="blackOutlined"
+        onClick={() => console.log("1st btn clicked")}
+      >
+        Black Outlined
+      </Button>
+
       <SocialButton
         storeName="Play Store"
         variant="filled"
+        onClick={() => console.log("clicked")}
+      />
+      <SocialButton
+        storeName="Apple Store"
+        variant="outlined"
         onClick={() => console.log("clicked")}
       />
     </div>
